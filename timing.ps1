4b69db62da5ad9a7bc90be0bb31f095fdf575b12
胡岩松 <shimuguyue513@qq.com>
2026-02-09 00:03:18 +0800

    Update 修改项目更新时间

--- timing.ps1
+++ timing.ps1
@@ -1,17 +1,19 @@
-# 获取当前绝对路径并拼接脚本路径
+# 获取路径
 $path = (Get-Location).Path
 $file = Join-Path $path "rebuild.ps1"
 
-# 定义操作：指定工作目录和执行参数
+# 定义操作
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' `
     -Argument "-ExecutionPolicy Bypass -File `"$file`"" `
     -WorkingDirectory $path
 
-# 定义触发器
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 2)
+# 定义每天凌晨 4 点的触发器
+$startTime = Get-Date -Hour 4 -Minute 0 -Second 0
+if ((Get-Date) -gt $startTime) { $startTime = $startTime.AddDays(1) }
+$trigger = New-ScheduledTaskTrigger -Daily -At $startTime
 
 # 注册任务
 Register-ScheduledTask -TaskName "VitePressAutoUpdate" -Action $action -Trigger $trigger -User "System" -RunLevel Highest -Force
-# 立即执行一次
-Start-ScheduledTask -TaskName "VitePressAutoUpdate"
-# 终止定时任务：Unregister-ScheduledTask -TaskName "VitePressAutoUpdate" -Confirm:$false
+
+# 删除任务
+# Unregister-ScheduledTask -TaskName "VitePressAutoUpdate" -Confirm:$false
